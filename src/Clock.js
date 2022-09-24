8a9ecb73bb96d82dd6dfd57ca3207199f20dc27a
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-09-25 02:34:49 +0300

    completed react hooks use effect cleanup

--- src/Clock.js
+++ src/Clock.js
@@ -4,10 +4,18 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerId = setInterval(() => {
       setTime(new Date());
     }, 1000);
+    
+    //returning a cleanup function
+    return function() {
+      clearInterval(timerId)
+    }
+
   }, []);
+
+  console.log("running")
 
   return <div>{time.toString()}</div>;
 }
